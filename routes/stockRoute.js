73eab48dbf4cc7e1806b7301037256cf6de0b0ce
Ameen-Here <johnharry125@yahoo.com>
2022-07-22 21:01:51 +0530

    Removed the chart trial page, fakeChart.ejs

--- routes/stockRoute.js
+++ routes/stockRoute.js
@@ -7,8 +7,10 @@ const {
   addStock,
 } = require("../controller/stockRoute");
 
+// Routing for portfolio page and asset adding confirmation page
 router.route("/portfolio").get(showPortfolio).post(addAsset);
 
+// Routing for adding asset to the database from the confirmation page
 router.post("/addStock", addStock);
 
 module.exports = router;
