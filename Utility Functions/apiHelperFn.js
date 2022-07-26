625135eae7ab64bea165bf79794944c1214f3038
Ameen-Here <johnharry125@yahoo.com>
2022-07-27 00:55:49 +0530

    Ready to deploy
    Added some responsiveness, made values private and heroku deployed,
    by connecting to mongo atlas.
    First deployment of this site

--- Utility Functions/apiHelperFn.js
+++ Utility Functions/apiHelperFn.js
@@ -4,10 +4,11 @@ const AppError = require("../AppError");
 
 const getSymbol = async function (companyName, exchange) {
   let url = "";
+  const apiKey = process.env.API_FINANCIAL;
   if (exchange === "nasdaq") {
-    url = `https://financialmodelingprep.com/api/v3/search?query=${companyName}&limit=10&exchange=NASDAQ&apikey=d8bf28c2dc51593dbdf880a34614b018`; // Get stocks symbol name used in stock exchange.
+    url = `https://financialmodelingprep.com/api/v3/search?query=${companyName}&limit=10&exchange=NASDAQ&apikey=${apiKey}`; // Get stocks symbol name used in stock exchange.
   } else {
-    url = `https://financialmodelingprep.com/api/v3/search?query=${companyName}&limit=10&exchange=NSE&apikey=d8bf28c2dc51593dbdf880a34614b018`; // Get stocks symbol name used in stock exchange.
+    url = `https://financialmodelingprep.com/api/v3/search?query=${companyName}&limit=10&exchange=NSE&apikey=${apiKey}`; // Get stocks symbol name used in stock exchange.
   }
 
   const response = await axios.get(url);
@@ -20,14 +21,15 @@ const getSymbol = async function (companyName, exchange) {
 
 const getCurPrice = async function (symbol, exchange) {
   let companyDetails;
+  const apiKey = process.env.API_ALPHA;
   if (exchange === "nasdaq") {
     console.log("here");
     companyDetails = await axios.get(
-      `https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=${symbol}&apikey=7EGYOYYJ63SX9WO9`
+      `https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=${symbol}&apikey=${apiKey}`
     );
   } else {
     companyDetails = await axios.get(
-      `https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=${symbol}.BSE&apikey=7EGYOYYJ63SX9WO9`
+      `https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=${symbol}.BSE&apikey=${apiKey}`
     ); // Getting company stock price of Indian market
   }
 
--- app.js
+++ app.js
@@ -108,4 +108,6 @@ app.use((err, req, res, next) => {
   res.status(status).send(message);
 });
 
-app.listen(3000, () => console.log("Listening to port 3000"));
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => console.log(`Listening to port ${port}`));
